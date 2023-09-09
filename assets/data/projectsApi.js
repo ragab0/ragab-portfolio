6a9c6f841f9dbb60fe462c214ca85a7f18485462
ragab0 <legendragab@gmail.com>
2023-09-10 01:46:14 +0300

    Fixing the broken link - re-arranging projects

--- assets/data/projectsApi.js
+++ assets/data/projectsApi.js
@@ -28,10 +28,17 @@ export const projectsApi = [
   },{
     "name": "First-React-Apps",
     "srcImg": FirstReactApps,
-    "link": "https://ste-two.onrender.com",
+    "link": "https://step-two.onrender.com",
     "gitLink": "https://github.com/ragab0/first-react-apps",
     "description": ["My first-react-apps that I had built, in order to learn and apply on React, React-Hooks, and React-Router-Dom."],
     "skills": ['HTML5', 'CSS3', 'JavaScript', 'JSON', 'React'],
+  },{
+    "name": "Ragab-Fitness",
+    "srcImg": Fitness,
+    "link": "https://ragab-fitness.vercel.app/",
+    "gitLink": "https://github.com/ragab0/Ragab-Fitness",
+    "description": [" عااااش يا بطـــل…", "For all young man whose i from them. I'm Ragab - the owner, the coach, and the student. I'm alsoo who create it :)", "You'll never need a coach after now as you can learn and choose your favourite plan. Don't worry of choosing a game into your plan as you'll play the best way, the right way", "I used the rapid-API to scrapping the games"],
+    "skills": ['HTML5', 'CSS3', "JavaScript", "React", "Nextjs", "Tailwindcss", "Rapid-API", "Figma"],
   },{
     "name": "Ragab-Resturant",
     "srcImg": Resturant,
@@ -40,26 +47,20 @@ export const projectsApi = [
     "description": ["My First modern full website that i wrote in React, And first full website i used webpack on, So its so closing to my heart.", "Although it the first code that i wrote in React, Its a good writin, and organized code"],
     "skills": ['HTML5', 'CSS3', "JavaScript", "React", "Figma"],
   },{
+    "name": "TODO-List",
+    "srcImg": todoList,
+    "link": "https://todo-tasks-manager.vercel.app",
+    "gitLink": "https://github.com/ragab0/todo-list",
+    "description": ["A todo-list, real todo-list, ", "Its a great written code you'll ever see in useReducer and useContext as i used them to states, suggest to take a look at", "Designed by me :)"],
+    "skills": ['HTML5', 'CSS3', "JavaScript", "JSON", "React", "Nextjs","Tailwindcss", "Figma"],
+    "inProgress": true
+  },{
     "name": "GPT-3",
     "srcImg": GPT3,
     "link": "https://ragab-gpt-3.vercel.app/",
     "gitLink": "https://github.com/ragab0/GPT-3",
     "description": ["A Modern website built in React and Nextjs, It is the place where you can know more about GPT-3, about the future", "Its a great code writin, and organized code so I suggest you to see the source-code with all love and respect for you :)"],
     "skills": ['HTML5', 'CSS3', "JavaScript", "React", "Nextjs", "Tailwindcss", "Figma"],
-  },{
-    "name": "HooBank",
-    "srcImg": HooBank,
-    "link": "https://ragab-hoo-bank.vercel.app/",
-    "gitLink": "https://github.com/ragab0/Hoo-Bank",
-    "description": ["Take care .. The Next Generation Payment Method. A Modern website where you can handle your business money. created in React and nextjs to practice on them"],
-    "skills": ['HTML5', 'CSS3', "JavaScript", "React", "Nextjs", "Tailwindcss", "Figma"],
-  },{
-    "name": "Ragab-Fitness",
-    "srcImg": Fitness,
-    "link": "https://ragab-fitness.vercel.app/",
-    "gitLink": "https://github.com/ragab0/Ragab-Fitness",
-    "description": [" عااااش يا بطـــل…", "For all young man whose i from them. I'm Ragab - the owner, the coach, and the student. I'm alsoo who create it :)", "You'll never need a coach after now as you can learn and choose your favourite plan. Don't worry of choosing a game into your plan as you'll play the best way, the right way", "I used the rapid-API to scrapping the games"],
-    "skills": ['HTML5', 'CSS3', "JavaScript", "React", "Nextjs", "Tailwindcss", "Rapid-API", "Figma"],
   },{
     "name": "Comphy-Store",
     "srcImg": store,
@@ -69,13 +70,12 @@ export const projectsApi = [
     "skills": ['HTML5', 'CSS3', "JavaScript", "JSON", "Redux", "React", "Nextjs","Tailwindcss", "Figma"],
     "inProgress": true
   },{
-    "name": "TODO-List",
-    "srcImg": todoList,
-    "link": "https://todo-tasks-manager.vercel.app",
-    "gitLink": "https://github.com/ragab0/todo-list",
-    "description": ["A todo-list, real todo-list, ", "Its a great written code you'll ever see in useReducer and useContext as i used them to states, suggest to take a look at", "Designed by me :)"],
-    "skills": ['HTML5', 'CSS3', "JavaScript", "JSON", "React", "Nextjs","Tailwindcss", "Figma"],
-    "inProgress": true
+    "name": "HooBank",
+    "srcImg": HooBank,
+    "link": "https://ragab-hoo-bank.vercel.app/",
+    "gitLink": "https://github.com/ragab0/Hoo-Bank",
+    "description": ["Take care .. The Next Generation Payment Method. A Modern website where you can handle your business money. created in React and nextjs to practice on them"],
+    "skills": ['HTML5', 'CSS3', "JavaScript", "React", "Nextjs", "Tailwindcss", "Figma"],
   },
 ]
 
