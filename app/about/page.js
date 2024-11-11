095eded0df2e434fdc8fbc9aed007f6be2814d4b
ragab0 <legendragab@gmail.com>
2024-11-11 21:54:42 +0200

    Freelancing version - removed about and contact pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,20 +1,5 @@
-import "./page.css";
-import Who from "./components/Who";
-import Experience from "./components/Experience";
-import Education from "./components/Education";
-import Skills from "./components/Skills";
-import Sidebar from "./components/Sidebar";
+import FreelanceVersion from "@/components/FreelanceVersion";
 
 export default function AboutPage() {
-  return (
-    <div className="about-page ">
-      <Sidebar />
-      <main className="about-paillers">
-        <Who />
-        <Experience />
-        <Education />
-        <Skills />
-      </main>
-    </div>
-  );
+  return <FreelanceVersion />;
 }
---
+++ components/FreelanceVersion.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FreelanceVersion() {
+  return (
+    <h1>
+      Freelancing version - not allowable to contact or to see perosnal info
+    </h1>
+  );
+}
